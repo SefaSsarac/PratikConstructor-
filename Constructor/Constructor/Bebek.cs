0c542e5eb66aa7c7bc422314db4a62b8bf98234f
Sefa Saraç <srcsfysf@gmail.com>
2024-08-07 19:39:53 +0300

    Add files via upload

---
+++ Constructor/Constructor/Bebek.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorPratik
+{
+    public class Bebek
+    {
+        // Property tanımlamaları
+        public DateTime DogumTarihi { get; set; }
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        // Default Constructor
+        public Bebek()
+        {
+            DogumTarihi = DateTime.Now;
+            Console.WriteLine($"Ingaaaaaaaaaaaaaaaaaaaa");
+
+        }
+        // Alternatif Constructor
+        public Bebek(string ad, string soyad)
+        {
+            Ad = ad;
+            Soyad = soyad;
+            DogumTarihi = DateTime.Now;
+
+            Console.WriteLine($"Ingaaaaaaaaaaaaaaaaaaaa");
+        }
+        // Bebek bilgilerini yazdırmak için bir metot
+        public void BilgileriYazdir()
+        {
+            Console.WriteLine($"AD: {Ad} SOYAD: {Soyad} DOĞUM TARİHİ: {DogumTarihi}");
+        }
+
+    }
+}
---
+++ Constructor/Constructor/Program.cs
@@ -0,0 +1,24 @@
+﻿using ConstructorPratik;
+using System.ComponentModel;
+using System.Threading.Channels;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        //Default Constructor ile bebek nesnesi oluşturma
+        Bebek bebek1 = new Bebek();
+
+        bebek1.Ad = "Serra";
+        bebek1.Soyad = "Yılmaz";
+        
+        bebek1.BilgileriYazdir();
+
+        // Alternatif Constructor ile bebek nesnesi oluşturma
+        Bebek bebek2 = new Bebek("Ahmet", "Kara");
+       
+        bebek2.BilgileriYazdir();
+    }     
+}
+
+
